47b7ff35ecacb2e9d155ecc904e4f3a7d91517c7
rufio39 <69258446+rufio39@users.noreply.github.com>
2020-09-22 13:39:47 -0600

    added videogames api route

--- app.js
+++ app.js
@@ -6,40 +6,51 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
-// 10-11 are modules that i can change.
-const indexRouter = require('./routes/index');
+const db = require('./data/mongo');
+
+//const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const videoGamesRouter = require('./routes/api/video-games');
 
 const app = express();
+db.connect(app.locals)
+  .then(() => {
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+    app.use(cors());
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    //app.use('/', indexRouter);
+    app.use('/v1/video-games',videoGamesRouter);
+    app.use('/users', usersRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+      next(createError(404));
+    });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+    // error handler
+    app.use(function (err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    process.on('SIGINT', () => {
+      db.close()
+      process.exit()
+    })
+     
+  })
 
 module.exports = app;
